eeb7062bb4e4650dd38a16f4726c5ad277d9cfb0
Rocky Hill <qbancoffee@yahoo.com>
2023-01-04 21:51:40 -0500

    Update drivewire4_windows_x86_64.bat

--- drivewire4_from_source/drivewire4_windows_x86_64.bat
+++ drivewire4_from_source/drivewire4_windows_x86_64.bat
@@ -1,3 +1,4 @@
 @echo off
 
-java -Djava.library.path=.\rxtx\windows -cp ./swt/linux/swt.jar;./dist/lib/;dist/dw4nb.jar com.groupunix.drivewireui.MainWin %*
+java -Djava.library.path=.\rxtx\windows -cp ./swt/windows/swt.jar;./dist/lib/;dist/dw4nb.jar com.groupunix.drivewireui.MainWin %*
+
